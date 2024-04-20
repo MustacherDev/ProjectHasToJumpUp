52d7285eed33f63bd09c8740b6f54edaf9c64e2c
Mustacher <114709957+MustacherDev@users.noreply.github.com>
2024-04-19 23:11:18 -0300

    Ondulando

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ var canvas = document.getElementById('Canvas');
 var ctx = canvas.getContext('2d');
 
 var largura = 10;
+var ang = 0;
 
 function step(){
     ctx.fillStyle = 'white';
-    ctx.fillRect(0,0, 300, 600);
+    ctx.fillRect(0,0, 800, 600);
+
 
 
     ctx.fillStyle = 'lightblue';
-    ctx.fillRect(50, 50, largura, 100);
+    ctx.fillRect(400 + 200*Math.sin(ang), 50, largura, 100);
     ctx.strokeStyle = 'blue';
     ctx.lineWidth = 5;
     ctx.strokeRect(50, 50, largura, 100);
@@ -18,6 +20,7 @@ function step(){
     ctx.fillText('Canvas Example', 50, 30);
 	
     largura++;
+	ang+= 0.05;
 
     requestAnimationFrame(step)
 }
