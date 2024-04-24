815113d7bfafc403f4ee5e0d26259c721fe5c510
Lucas Werthein <105745101+LucasWertheinSantos@users.noreply.github.com>
2024-04-23 23:34:44 -0300

    avanços em dias
    ta começando a surgir algo legal. algo de ruim irá acontecer em breve.

--- script.js
+++ script.js
@@ -6,29 +6,76 @@ var largura = 10;
 var ang = 0;
 var mofo = new Mofo(canvas.width/2, canvas.height/2);
 
+function ball(){
+        
+        this.x = canvas.width / 2;
+        this.y = canvas.height - 30;
+        this.dx = 5;
+        this.dy = -5;
+        this.ballRadius = 10;
+
+        this.drawBall = function () {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.ballRadius, 0, Math.PI * 2);
+            ctx.fillStyle = '#0095DD';
+            ctx.fill();
+            ctx.closePath();
+        }
+
+        this.draw = function (plataforma) {
+            this.drawBall();
+
+            // Verifica se a bola atingiu as paredes do canvas
+            if (this.x + this.dx > canvas.width - this.ballRadius || this.x + this.dx < this.ballRadius) {
+              this.dx = -this.dx; // Inverte a direção horizontal
+            }
+            if (this.y + this.dy > canvas.height - this.ballRadius || this.y + this.dy < this.ballRadius) {
+              this.dy = -this.dy; // Inverte a direção vertical
+            }
+            else if (this.y + this.dy > canvas.height - this.ballRadius) {
+              // Verifica colisão com a plataforma
+                if (this.x > plataforma.x && this.x < plataforma.x + plataforma.width) {
+                  this.dy = -this.dy;
+                }
+            }
+            this.x += this.dx;
+            this.y += this.dy;
+        }
+      }
+function plataforma (x,y){
+    this.x = x;
+    this.y = y;
+    this.dx = 3;
+    this.width = 100;
+    this.height = 15;
+
+    this.draw = function(){
+
+      ctx.fillStyle = '#000000';
+      ctx.fillRect(this.x,this.y,this.width,this.height);
+      
+    }
+    this.update = function () {
+      if (this.x + this.dx > canvas.width - this.width || this.x + this.dx < 0) {
+        this.dx = -this.dx; // Inverte a direção horizontal
+      }
+
+      this.x += this.dx;
+    }
+
+}
+  var rectangle = new plataforma ((canvas.width -100)/2,canvas.height -50);
+  var ball = new ball();
+
 function step(){
 
   // Fundo Branco
   ctx.fillStyle = 'white';
   ctx.fillRect(0,0, canvas.width, canvas.height);
 
-  ctx.fillStyle = 'lightblue';
-  ctx.fillRect(400 + 200*Math.sin(ang), 50, largura, 100);
-  ctx.strokeStyle = 'blue';
-  ctx.lineWidth = 5;
-  ctx.strokeRect(50, 50, largura, 100);
-  ctx.font = '20px Arial';
-  ctx.fillStyle = 'black';
-  ctx.fillText('Canvas Example', 50, 30);
-
-  largura++;
-  ang+= 0.05;
-
-  // Tá mofado
-  // Triste
-  mofo.update();
-  mofo.draw();
-
+  rectangle.update();
+  rectangle.draw();
+  ball.draw(rectangle);
   requestAnimationFrame(step)
 }
 
